5f0fa37c9b94cb9f63cd0905ecc7cc92589aeacd
Akash Varshney <akash.varshney@arcadis.com>
2023-07-26 22:01:49 +0530

    intial Comitt

---
+++ job/src/main/java/com/texhibit/job/entities/ExperienceRange.java
@@ -0,0 +1,11 @@
+package com.texhibit.job.entities;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class ExperienceRange {
+    private Integer expFrom;
+    private Integer expTo;
+}
