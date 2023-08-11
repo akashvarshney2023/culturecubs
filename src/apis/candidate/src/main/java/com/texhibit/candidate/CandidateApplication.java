4f4701d4b4d735200985557b7610f7daa5699c79
akashvarshney2023 <akashvarshney2023@outlook.com>
2023-08-11 21:43:41 +0530

    udpated code

--- src/apis/candidate/src/main/java/com/texhibit/candidate/CandidateApplication.java
+++ src/apis/candidate/src/main/java/com/texhibit/candidate/CandidateApplication.java
@@ -1,18 +1,11 @@
 package com.texhibit.candidate;
 
-import org.springdoc.core.SwaggerUiConfigParameters;
-import org.springdoc.core.SwaggerUiConfigParametersBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springdoc.core.SpringDocConfigProperties;
-import org.springdoc.core.SwaggerUiConfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springdoc.core.SwaggerUiConfigParametersBuilder; // Make sure this import is present
-import org.springdoc.core.EnableOpenApi; // Make sure this import is present
 
 @SpringBootApplication
 @OpenAPIDefinition
-@EnableOpenApi
 public class CandidateApplication {
 
 	public static void main(String[] args) {
