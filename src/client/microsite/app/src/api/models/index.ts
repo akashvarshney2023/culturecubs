aff1f111560e03a166a920e40fa0c5a72b9afb52
akashvarshney2023 <akashvarshney2023@outlook.com>
2023-08-15 16:50:24 +0530

    Added openapi documentation for candidate on microsite

---
+++ src/client/microsite/app/src/api/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Candidate';
+export * from './CandidateDto';
+export * from './CertificationsAndLicense';
+export * from './ContactInformation';
+export * from './Education';
+export * from './PersonalInformation';
+export * from './Reference';
+export * from './WorkExperience';
