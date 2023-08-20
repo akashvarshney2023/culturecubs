2ff4b6936cc3c34ec31cf360ee1edc37e0338367
akashvarshney2023 <akashvarshney2023@outlook.com>
2023-08-20 15:16:50 +0530

    Updated code for settings page

--- src/client/admin/app/src/views/settings/Settings.vue
+++ src/client/admin/app/src/views/settings/Settings.vue
@@ -1,44 +1,57 @@
-
 <template>
     <v-main>
-        <v-layout style="min-height:89%;">
-            <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
-                <v-list-item title="Microsoft" nav>
-                    <template v-slot:append>
-                        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
-                    </template>
-                    <template v-slot:prepend>
-                        <img src="https://www.microsoft.com/favicon.ico?v2" alt="Microsoft Logo" class="avatar-image">
-                    </template>
-                </v-list-item>
-                <v-divider></v-divider>
-                <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-group-outline" title="candidates" value="users"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-multiple-plus" title="participants"
-                        value="Participants"></v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-            <v-main style="height: 250px"></v-main>
-        </v-layout>
+      <v-layout style="min-height: 89%;">
+        <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+          <v-list-item title="Microsoft" nav>
+            <template v-slot:append>
+              <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+            </template>
+            <template v-slot:prepend>
+              <img src="https://www.microsoft.com/favicon.ico?v2" alt="Microsoft Logo" class="avatar-image">
+            </template>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-account" title="My Account" value="account" @click="setActiveComponent('account')"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-group-outline" title="candidates" value="users" @click="setActiveComponent('users')"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-multiple-plus" title="participants" value="Participants" @click="setActiveComponent('participants')"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-main>
+          <!-- Conditionally render components based on the active component -->
+          <MicositeView v-if="activeComponent === 'account'" />
+          <CandidateView v-if="activeComponent === 'users'" />
+          <ContentView v-if="activeComponent === 'participants'" />
+        </v-main>
+      </v-layout>
     </v-main>
-</template>
-<script lang="ts" setup>
-import { ref } from 'vue';
-
-
-const drawer = ref(true);
-const rail = ref(true);
-
-</script>
-<style scoped>
-.avatar-image {
+  </template>
+  
+  <script lang="ts" setup>
+  import { ref } from 'vue';
+  import CandidateView from '@/views/settings/options/CandidateView.vue';
+  import MicositeView from '@/views/settings/options/MicrositeView.vue';
+  import ContentView from '@/views/settings/options/ContentView.vue';
+  
+  const drawer = ref(true);
+  const rail = ref(true);
+  const activeComponent = ref(null); // Initialize with null
+  
+  const setActiveComponent = (value) => {
+    activeComponent.value = value;
+  };
+  </script>
+  
+  <style scoped>
+  .avatar-image {
     width: auto;
     /* Adjust the width as needed */
     height: 24px;
     /* Adjust the height as needed */
     border-radius: 0;
     /* Remove circular border radius */
-    padding-right : 10px;
-    padding-left : 10px;
-}</style>
+    padding-right: 10px;
+    padding-left: 10px;
+  }
+  </style>
+  
---
+++ src/client/admin/app/src/views/settings/options/CandidateView.vue
@@ -0,0 +1,8 @@
+<template>
+    <h4>This is candidate settings page</h4>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style>
+</style>
