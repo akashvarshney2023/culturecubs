a3c39ea27a3ab6f7700e04af39e98446b52dbf75
Akash Varshney <akashvarshney2023@outlook.com>
2023-11-11 21:36:40 +0000

    added Logo icon click

--- src/client/microsite/app/src/views/shared/_Layout.vue
+++ src/client/microsite/app/src/views/shared/_Layout.vue
@@ -2,8 +2,8 @@
   <header class="sticky-top custom-navbar">
     <v-app-bar class="bg-white" density="default" :elevation="0"
       style="border-bottom:1px solid #e0e0e0;padding:0px 10% !important;">
-      <v-app-bar-title>
-        <v-img :src="logo.tenantIcon" style="width:350px;height:54px;"></v-img>
+      <v-app-bar-title @click="navigate('home')">
+        <v-img :src="logo.tenantIcon" style="width:350px;height:54px; cursor: pointer;"></v-img>
       </v-app-bar-title>
       <v-tabs color="primary" v-model="activetab">
         <v-tab value="home" @click="navigate('home')">Home</v-tab>
