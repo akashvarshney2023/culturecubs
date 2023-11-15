65797e88f29e3cdb4a40196d31bad91623a0cc50
Akash Varshney <akashvarshney2023@outlook.com>
2023-11-15 19:31:34 +0000

    Updated some code for validating env variables

--- src/client/microsite/app/src/views/RegisterView.vue
+++ src/client/microsite/app/src/views/RegisterView.vue
@@ -12,12 +12,11 @@
             <v-card-title class="text-center">Register</v-card-title>
             <v-card-text>
               <v-form @submit.prevent="submitForm">
-                <v-text-field label="Company" type="text" v-model="company" prepend-icon="mdi-domain"
-                  required></v-text-field>
-                <v-text-field label="User" type="text" v-model="user" prepend-icon="mdi-account" required></v-text-field>
+                <v-text-field label="Full Name" type="text" v-model="user" prepend-icon="mdi-account" required></v-text-field>
                 <v-text-field label="Email" type="email" v-model="email" prepend-icon="mdi-email" required></v-text-field>
                 <v-text-field label="Phone" type="tel" v-model="phone" prepend-icon="mdi-phone" required></v-text-field>
-                <v-text-field label="URL" type="url" v-model="url" prepend-icon="mdi-link" required></v-text-field>
+                <v-text-field label="Company" type="text" v-model="company" prepend-icon="mdi-domain" required></v-text-field>
+                <v-file-input label="Resume Attachment (docx, pdf)" v-model="attachment" accept=".docx, .pdf" @change="handleAttachment"></v-file-input>
                 <v-checkbox label="I agree to the terms and conditions of Curlture Cubs and am happy to sign this"
                   v-model="agree"></v-checkbox>
                 <v-row class="d-flex justify-center">
@@ -29,7 +28,7 @@
                     <v-btn color="secondary" block>Cancel</v-btn>
                   </v-col>
                 </v-row>
-              </v-form>             
+              </v-form>
             </v-card-text>
           </v-card>
         </v-col>
@@ -49,16 +48,17 @@ import type { PersonalInformation, Candidate } from '@/apis/candidate/models';
 
 export default defineComponent({
   setup() {
+  
     const router = useRouter();
     const candidateApi = new CandidateApi();
 
-    const company = ref('');
     const user = ref('');
     const email = ref('');
     const phone = ref('');
-    const url = ref('');
-    const attachment: Ref<File | null> = ref(null);  // Stores the selected attachment file
+    const company = ref('');
+    const attachment: Ref<File | null> = ref(null);
     const agree = ref(false);
+
 
     const handleAttachment = (file: File) => {
       attachment.value = file;
@@ -66,9 +66,31 @@ export default defineComponent({
 
     const submitForm = async () => {
       if (attachment.value) {
+        const emailWithoutSpecialChars = email.value.replace(/[^a-zA-Z0-9]/g, '');
+        const fileName = `Resume_${emailWithoutSpecialChars}_${Date.now()}`;
+        const formData = new FormData();
+        formData.append('file', attachment.value, fileName);
+        try {
+          // Upload the file to Azure Blob storage
+          // You need to implement the Azure Blob storage upload logic here using Azure SDK or HTTP requests.
 
+          // Example using Fetch API (replace with your Azure Blob storage upload logic):
+         
+          const uploadResponse = await fetch('YOUR_AZURE_BLOB_STORAGE_URL', {
+            method: 'PUT',
+            body: formData,
+          });
+
+          if (!uploadResponse.ok) {
+            throw new Error('File upload failed');
+          }
+
+          // Continue with the rest of your form submission logic...
+        } catch (error) {
+          console.error('File upload failed:', error);
+        }
       }
-      const registerUser = async()=>{
+      const registerUser = async () => {
 
       }
       //Create a new candidate regsitration details 
@@ -91,7 +113,6 @@ export default defineComponent({
         // Call the API to save the candidate registration
 
 
-
       } catch (error) {
         console.error('Save Failed:', error);
       }
@@ -111,7 +132,7 @@ export default defineComponent({
       agree,
       handleAttachment,
       submitForm,
-      
+
     };
   },
 });
