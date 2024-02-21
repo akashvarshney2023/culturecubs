37cc75c8ac875eb2f0210021196134161b5d1f30
Akash Varshney <akashvarshney2023@outlook.com>
2024-02-21 20:58:56 +0000

    Update Background for luksup

--- src/client/microsite/app/src/views/main/HomeView.vue
+++ src/client/microsite/app/src/views/main/HomeView.vue
@@ -1,55 +1,50 @@
 <template>
-    <v-main class="h-screen m-b5">
-      <v-container>
-        <v-row style="justify-content:flex-end;padding:0% 8%;">
-          <v-col class="pt-0">
-            <v-card>
-              <v-img src="../../assets/images/background.jpg" style="height:350px;" cover>
-                <div style="color:white;display:grid;position:absolute;bottom:0;padding:10px;">
-                  <span class="text-h1" style="font-weight:700;">Get Familiar,</span>
-                  <span class="text-h1" style="font-weight:500;">Then Hire</span>
-                  <span class="text-h4">Program to find Talent..</span>
-                </div>
-              </v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row style="justify-content:flex-end;padding:0% 8% ;">
-          <v-col class="pt-0">
-            <v-card style="background-color:;">
-              <div class="pb-5" style="font-weight:bold; font-family: Verdana, Geneva, Tahoma, sans-serif;">
-                <i>When we say "get familiar," we don't just mean reviewing resumes and conducting interviews. We take a
-                  more
-                  holistic approach that involves getting to know candidates on a personal level. We believe that this helps
-                  us to understand their motivations, work style, and overall fit with our team.</i>
-              </div>
-              <div class="pb-5" style="font-weight:bold; font-family: Verdana, Geneva, Tahoma, sans-serif;">
-                <i>We often invite potential employees to spend time with us on-site, working alongside our team members to
-                  get
-                  a sense of our company culture and work environment. We also encourage candidates to ask questions and
-                  share
-                  their own experiences and insights with us.</i>
-              </div>
-              <i></i>
-            </v-card>
-            <v-card>
-              <div class="pb-5" style="font-weight:bold;">
-  
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </template>
-  
-  <script lang="ts" setup>
-  
+  <v-main class="h-screen m-b5">
+    <v-container>
+      <v-row style="justify-content:flex-end;padding:0% 8%;">
+        <v-col class="pt-0">
+          <v-card>
+            <v-img src="../../assets/images/landingpageluksup1.png" style="height:350px;" cover>
+              <div style="color:white;display:grid;position:absolute;bottom:0;padding:10px;">
+                <span class="text-h1" style="font-weight:450;">Bring Fun Life To</span>
+                <span class="text-h1" style="font-weight:350;">Your Kids</span>
 
-  </script>
+              </div>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row style="justify-content:flex-end;padding:0% 8% ;">
+        <v-col cols="12" sm="6" class="pt-0" style="text-align:center;">
+          <v-card elevation="0">
+            <div style="font-family: monospace; font-size: 22px; color: black;" >
+              <b>LukzUp</b> is a unique and upcoming (yet to be released) webstore designed to cater to the needs of
+              growing
+              children. The kids' category is estimated to reach a market size of <b>USD 35B by 2025</b> in India, and we
+              are poised to lead in this space.
+              <br><br>
+              The needs of a growing child are different from an infant/toddler and very distinct from an adolescent. We
+              believe that strong curation, context-specific search, easy discovery, strong personalization, and group
+              buying can create a differentiated buying experience for parents and their children.
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" class="pt-0">
+          <v-card elevation="0">
+            <v-img src="../../assets/images/lukzuphome2.png" style="height:350px;" cover></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
   
-  <style scoped> 
-  .v-container{
-    max-width: max-content;
-  }
-  </style>
+<script lang="ts" setup>
+
+
+</script>
+  
+<style scoped> .v-container {
+   max-width: max-content;
+ }
+</style>
